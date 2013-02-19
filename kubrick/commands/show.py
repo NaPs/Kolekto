254d646ca48a1bce1e436c917e5b917861e96d8f
Antoine Millet <antoine@inaps.org>
2013-02-19 22:20:22 +0100

    Added a sorter for metadata keys in show command

--- kubrick/commands/show.py
+++ kubrick/commands/show.py
@@ -8,10 +8,14 @@ from kubrick.printer import printer
 from fabulous.color import bold
 
 
+METADATA_SORTER_FIRST = ('title', 'year', 'directors', 'cast', 'writers',
+                         'collection', 'genres')
+
+
 def show(movie):
     """ Show the movie metadata.
     """
-    for key, value in movie.iteritems():
+    for key, value in sorted(movie.iteritems(), cmp=metadata_sorter, key=lambda x: x[0]):
         if isinstance(value, list):
             if not value:
                 continue
@@ -22,6 +26,28 @@ def show(movie):
         printer.p('{key}: {value}', key=bold(key), value=value)
         for value in other:
             printer.p('{pad}{value}', value=value, pad=' ' * (len(key) + 2))
+
+
+def metadata_sorter(x, y):
+    """ Sort metadata keys by priority.
+    """
+    if x == y:
+        return 0
+    if x in METADATA_SORTER_FIRST and y in METADATA_SORTER_FIRST:
+        return -1 if METADATA_SORTER_FIRST.index(x) < METADATA_SORTER_FIRST.index(y) else 1
+    elif x in METADATA_SORTER_FIRST:
+        return -1
+    elif y in METADATA_SORTER_FIRST:
+        return 1
+    else:
+        if x.startswith('_') and y.startswith('_'):
+            return cmp(x[1:], y[1:])
+        elif x.startswith('_'):
+            return 1
+        elif y.startswith('_'):
+            return -1
+        else:
+            return cmp(x, y)
 
 
 class Show(Command):
