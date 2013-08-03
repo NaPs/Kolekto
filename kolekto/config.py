c2a21a8a90d593dd370843c9313f2b19c4b02d9d
Antoine Millet <antoine@inaps.org>
2013-08-03 22:39:47 +0200

    Added profile key in configuration
    Also implemented a "Profile" Dotconf type which is able to load profiles.

--- kolekto/config.py
+++ kolekto/config.py
@@ -1,9 +1,33 @@
 """ Kolekto configuration parsers.
 """
 
+import pkg_resources
+
 from dotconf import Dotconf
+from dotconf.schema import ValidationError
 from dotconf.schema.containers import Section, Value
 from dotconf.schema.types import String
+
+from .profiles.movies import Movies
+
+
+class Profile(String):
+
+    """ Type for Kolekto profiles.
+
+    This type load know profiles using distutils entry-points.
+    """
+
+    def validate(self, value):
+        value = super(Profile, self).validate(value)
+        # Get the first profile to match the provided value:
+        profile = next(pkg_resources.iter_entry_points('kolekto.profiles', str(value)), None)
+        if profile is None:
+            raise ValidationError('Unknown profile')
+        return profile.name, profile.load()
+
+    def cast(self, value):
+        raise NotImplementedError()
 
 
 class ViewKolektoConfig(Section):
@@ -25,6 +49,7 @@ class DatasourceKolektoConfig(Section):
 
 class RootKolektoConfig(Section):
 
+    profile = Value(Profile(), default=('movies', Movies))
     view = ViewKolektoConfig()
     datasource = DatasourceKolektoConfig()
 
