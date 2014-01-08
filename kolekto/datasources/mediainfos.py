dd89e6643e8d0ef4bbc57051752a7504f8077331
Antoine Millet <antoine@inaps.org>
2014-01-08 22:56:04 +0100

    Implemented cache in media infos datasource
    This change will allow to use the attach feature of datasources in list and
    will speed up the link and stats operations.

--- kolekto/datasources/mediainfos.py
+++ kolekto/datasources/mediainfos.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from kolekto.datasources import Datasource
+from kolekto.helpers import JsonDbm
 
 import kaa.metadata
 
@@ -11,26 +12,38 @@ metadata_logger.setLevel(logging.CRITICAL)
 
 class MediainfosDatasource(Datasource):
 
+    def __init__(self, *args, **kwargs):
+        super(MediainfosDatasource, self).__init__(*args, **kwargs)
+        # Open the media infos cache:
+        cache_filename = os.path.join(self.tree, '.kolekto', 'media-info-cache.db')
+        self._cache = JsonDbm(cache_filename)
+
     def attach(self, movie_hash, movie):
-        filename = os.path.join(self.tree, '.kolekto', 'movies', movie_hash)
-        infos = kaa.metadata.parse(filename)
-        if infos is None:
-            return movie
-
-        movie['container'] = infos['type'].strip()
-
-        # Set the quality of the video depending on its definition:
-        if infos.video[0].width < 1280:
-            movie['quality'] = 'SD'
-        elif 1280 <= infos.video[0].width < 1920:
-            movie['quality'] = '720p'
+        if movie_hash in self._cache:
+            media_infos = self._cache.get(movie_hash)
         else:
-            movie['quality'] = '1080p'
+            filename = os.path.join(self.tree, '.kolekto', 'movies', movie_hash)
+            infos = kaa.metadata.parse(filename)
+            if infos is None:
+                return movie
 
-        # Set the file extension depending on its mimetype:
-        movie['ext'] = infos['mime'].split('/')[-1]
+            media_infos = {}
+            media_infos['container'] = infos['type'].strip()
 
-        # Set the movie length (in minutes)
-        movie['runtime'] = int(infos['length'] / 60)
+            # Set the quality of the video depending on its definition:
+            if infos.video[0].width < 1280:
+                media_infos['quality'] = 'SD'
+            elif 1280 <= infos.video[0].width < 1920:
+                media_infos['quality'] = '720p'
+            else:
+                media_infos['quality'] = '1080p'
 
+            # Set the file extension depending on its mimetype:
+            media_infos['ext'] = infos['mime'].split('/')[-1]
+
+            # Set the movie length (in minutes)
+            media_infos['runtime'] = int(infos['length'] / 60)
+            self._cache.save(movie_hash, media_infos)
+
+        movie.update(media_infos)
         return movie
