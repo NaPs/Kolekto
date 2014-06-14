704f6465a3f54282803f5fbbc84000e26dcec2e0
Antoine Millet <antoine@inaps.org>
2014-06-14 15:40:27 +0200

    Now show quality on output of find-duplicates

--- kolekto/commands/find_duplicates.py
+++ kolekto/commands/find_duplicates.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 from kolekto.printer import printer
 from kolekto.commands import Command
+from kolekto.datasources import MovieDatasource
 
 
 class FindDuplicates(Command):
@@ -12,10 +13,13 @@ class FindDuplicates(Command):
     help = 'find duplicate movies in collection'
 
     def run(self, args, config):
+        mds = MovieDatasource(config.subsections('datasource'), args.tree, self.profile.object_class)
         mdb = self.get_metadata_db(args.tree)
         hash_by_title = defaultdict(lambda: [])
         for movie_hash, movie in mdb.itermovies():
-            hash_by_title[movie.get('title', None), movie.get('year', None)].append(movie_hash)
+            movie = mds.attach(movie_hash, movie)
+            hash_info = '<inv> %s </inv> (%s/%s)' % (movie_hash, movie.get('quality'), movie.get('ext'))
+            hash_by_title[movie.get('title', None), movie.get('year', None)].append(hash_info)
 
         for (title, year), hashs in hash_by_title.iteritems():
             if len(hashs) > 1:
